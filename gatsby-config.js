cb0f5b63ae2d74553aa3c06ce080025c31ffd797
Jose Mendoza <developermendoza@gmail.com>
2022-07-13 14:16:10 -0700

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: `My First Gatsby Site`,
+    title: `Gatsby Website`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
---
+++ src/pages/blog.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+
+const BlogPage = ({ data }) => {
+  console.log("data: ", data);
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+
+export default BlogPage;
